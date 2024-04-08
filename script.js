eaf4b56fa3d53525bba1384a8b99bfeac5e8a1af
priyanshudayal1 <priyanshudayal1504@gmail.com>
2024-04-08 12:17:32 +0530

    completed

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+function locomotive() {
+    gsap.registerPlugin(ScrollTrigger);
+    const locoScroll = new LocomotiveScroll({
+        el: document.querySelector("#main"),
+        smooth: true
+    });
+    locoScroll.on("scroll", ScrollTrigger.update);
+    ScrollTrigger.scrollerProxy("#main", {
+        scrollTop(value) {
+            return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+        }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+        getBoundingClientRect() {
+            return { top: 0, left: 0, width: window.innerWidth, height: window.innerHeight };
+        },
+        // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+        pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+    });
+
+
+
+
+    // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+    ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+    // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+    ScrollTrigger.refresh();
+
+}
+
 function navAnimation() {
     let nav = document.querySelector("nav");
     nav.addEventListener("mouseenter", () => {
@@ -63,6 +92,21 @@ function pageTwoAnimation() {
             })
         })
     })
+    let video = document.querySelectorAll("#page7 video");
+    let sections = document.querySelectorAll(".sec-right");
+
+    sections.forEach((sec) => {
+        let video = sec.childNodes[3]
+        sec.addEventListener("mouseenter", () => {
+            video.style.opacity = 1;
+            video.play();
+        })
+        sec.addEventListener("mouseleave", () => {
+            video.style.opacity = 0;
+            video.load();
+        })
+    })
+
 };
 
 function page3VideoAnimation() {
@@ -86,25 +130,74 @@ function page3VideoAnimation() {
             borderRadius: "30px"
         })
     })
+
 }
 
-let video=document.querySelectorAll("#page7 video");
-let sections=document.querySelectorAll(".sec-right");
-
-sections.forEach((sec)=>{
-    let video=sec.childNodes[3]
-    sec.addEventListener("mouseenter",()=>{
-        video.style.opacity=1;
-        video.play();
+function page9Animation() {
+    gsap.from("#btm6-part2 h4", {
+        x: 0,
+        duration: 1,
+        scrollTrigger: {
+            trigger: "#btm6-part2",
+            scroller: "#main",
+            start: "top 80%",
+            end: "top 20%",
+            scrub: true
+        }
     })
-    sec.addEventListener("mouseleave",()=>{
-        video.style.opacity=0;
-        video.load();
+}
+
+
+
+
+function loadingAnimation() {
+    var tl = gsap.timeline();
+
+    tl.from("#page1", {
+        opacity: 0,
+        duration: 0.3,
+        delay: -0.2
     })
-})
+
+    tl.from("#page1", {
+        transform: "scaleX(0.7) scaleY(0.2) translateY(80%)",
+        borderRadius: "50px",
+        duration: 2,
+        ease: "expo.out"
+    });
+
+    tl.from("nav", {
+        opacity: 0
+    })
+
+    tl.from("#page1 h1,#page1 p,#page1 div", {
+        opacity: 0,
+        duration: 0.5,
+        stagger: 0.2
+    })
+
+}
+
+// gsap.to("#btm6-part2 h4", {
+//     x: 150,
+//     duration: 1,
+//     stagger: {
+//         amount: -0.3
+//     },
+//     scrollTrigger: {
+//         trigger: "#btm6-part2",
+//         scroller: "body",
+//         start: "top 80%",
+//         end: "top -80",
+//         scrub: true
+//     }
+// })
 
 
 
+locomotive();
+loadingAnimation();
 navAnimation();
 pageTwoAnimation();
 page3VideoAnimation();
+page9Animation();
